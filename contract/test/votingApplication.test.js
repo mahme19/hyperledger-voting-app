3c3fb8484e01e69034440fef912a8ab558eb2738
mahme19 <mahme19@student.sdu.dk>
2022-12-04 22:20:22 +0100

    test fix

--- contract/test/votingApplication.test.js
+++ contract/test/votingApplication.test.js
@@ -20,12 +20,13 @@ describe('VotingContract', () => {
     let stub;
     let ctx;
 
-    beforeEach(() => {
+    beforeEach(async () => {
         contract = new VotingContract();
         stub = sinon.createStubInstance(ChaincodeStub);
 
         ctx = new Context();
         ctx.setChaincodeStub(stub);
+        
         voter1 = '{"voterId":"TestVoter1, "name": "Test1"}'
         voter2 = '{"voterId":"TestVoter2, "name": "Test2"}'
         ctx.stub.getState.withArgs('TestVoter1').resolves(Buffer.from(voter1));
@@ -35,11 +36,12 @@ describe('VotingContract', () => {
     describe('Voter exist', () => {
 
         it('should return true for a voter', async () => {
-            await contract.voterExists(ctx, 'TestVoter1').should.eventually.be.true;
+            await contract.myAssetExists(ctx, 'TestVoter1').should.eventually.be.true;
+            
         });
 
         it('should return false for a voter that does not exist', async () => {
-            await contract.voterExists(ctx, 'TestVoter3').should.eventually.be.false;
+            await contract.myAssetExists(ctx, 'TestVoter3').should.eventually.be.false;
         });
 
     });
